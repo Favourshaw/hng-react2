be1ac4e8a38c510dc80e4ebcb2a24d49185e1262
Favourshaw <favourvanderwaal@gmail.com>
2022-11-05 16:09:54 +0100

    feat: form validaion

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,25 @@ import { useForm } from 'react-hook-form'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 export function Contact() {
+  (function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
     const {register, handleSubmit} = useForm();
     const onSubmit = (d) =>
     alert(JSON.stringify(d));
@@ -11,15 +30,19 @@ export function Contact() {
         <div className="container mt-5 pt-5 col-sm-6" >
   <h2>Contact Me</h2>
   <p>Hi there, contact me to ask me about anything you have in mind.</p>
-  <form onSubmit={handleSubmit(onSubmit)}>
+  <form onSubmit={handleSubmit(onSubmit)} class="was-validated" >
     <div className="row " >
       <div className="col mb-3 mt-3">
       <label for="fname">First name</label>
-        <input type="text" className="form-control" id='first_name' placeholder="Enter your first name" {...register("fname")} required/>
+        <input type="text" className="form-control"  id='first_name'  {...register("fname")} required/>
+        
+      <div class="invalid-feedback">Please fill out this field.</div>
       </div>
       <div className="col mb-3 mt-3">
       <label for="lname">Last name</label>
         <input type="text" className="form-control" id='last_name' placeholder="Enter your last name" {...register("lname")} required/>
+        
+      <div class="invalid-feedback">Please fill out this field.</div>
       </div>
     </div>
     
@@ -33,12 +56,14 @@ export function Contact() {
     <div className="row">
       <div className="col mb-3 mt-3">
         <label for="message">Message</label>
-<textarea className="form-control" rows="5" id="message" {...register("message")} placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
+<textarea className="form-control " rows="5" id="message" {...register("message")} defaultValue="Hey Favour, hope you are doing great. Let us collaborate on project axyz." placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
+
+      <div class="invalid-feedback txt">Please fill out this field.</div>
       </div>
     </div>
     
     <div className="form-check mb-3 mt-3">
-      <input type="checkbox" className="form-check-input" id="check2" {...register("chk")} value="something" required/>
+      <input type="checkbox" className="form-check-input" id="check2" {...register("chk", { required: true })} value="something" />
       <label className="form-check-label" for="check2">You agree to providing your data to Favour who may contact you.</label>
     </div>
     
@@ -46,7 +71,7 @@ export function Contact() {
     <div className="row">
       <div className="col mb-3 mt-3">
         <div className="d-grid">
-    <button type="submit" value="submit" id='btn__submit' className="btn btn-primary btn-block">Send message </button>
+    <button type="submit" id='btn__submit' className="btn btn-primary btn-block">Send message </button>
   </div>
       </div>
     </div>
